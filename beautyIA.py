d1ee7492076ab93811557a04ad17e8511d04b953
m4ite <maitesenaibosch@gmail.com>
2023-11-24 08:54:00 -0300

    beauty ai

---
+++ beautyIA.py
@@ -0,0 +1,127 @@
+from keras import models
+from keras import layers
+from keras import activations
+from keras import initializers
+from keras import regularizers
+from keras import metrics
+from keras import optimizers
+from keras import losses
+from keras import callbacks
+from keras.preprocessing import image
+
+model = models.Sequential()
+
+model.add(layers.Conv2D(
+    128,
+    (5,5),
+    input_shape=(128,128,3),
+    activation='relu'
+))
+
+model.add(layers.MaxPooling2D(
+    pool_size = (2, 2) 
+))
+
+model.add(layers.Conv2D(
+    64,
+    (5,5),
+    input_shape=(62,62,3),
+    activation='relu'
+))
+
+model.add(layers.MaxPooling2D(
+    pool_size = (2, 2) 
+))
+
+model.add(layers.Conv2D(
+    32,
+    (5,5),
+    input_shape=(29,29,3),
+    activation='relu'
+))
+
+model.add(layers.MaxPooling2D(
+    pool_size = (2, 2)
+))
+
+model.add(layers.Conv2D(
+    16,
+    (5,5),
+    input_shape=(13,13,3),
+    activation='relu'
+))
+
+model.add(layers.MaxPooling2D(
+    pool_size = (2, 2)
+))
+
+
+
+model.add(layers.Flatten())
+
+model.add(layers.Dense(
+    256,
+    kernel_initializer= initializers.RandomNormal(stddev=1), 
+    bias_initializer= initializers.Zeros(),
+    activation=activations.relu
+))
+
+
+model.add(layers.Dense(
+    128,
+    kernel_initializer= initializers.RandomNormal(stddev=1), 
+    bias_initializer= initializers.Zeros(),
+    activation=activations.relu
+))
+
+
+model.add(layers.Dense(
+    2,
+    kernel_initializer= initializers.RandomNormal(stddev=1),
+    bias_initializer= initializers.Zeros(),
+    activation=activations.softmax
+))
+
+
+model.compile(
+  optimizer=optimizers.Adam(),
+  loss= losses.CategoricalCrossentropy(),
+  metrics = [metrics.CategoricalAccuracy()]
+)
+
+dataGen = image.ImageDataGenerator(
+    rescale=1.0/255,
+    shear_range=0.2,
+    zoom_range=0.2,
+    horizontal_flip=True,
+    vertical_flip=False,
+    validation_split=0.2
+)
+
+
+X_train = dataGen.flow_from_directory(
+    "beauty/",
+    target_size=(128,128),
+    batch_size=10,
+    class_mode='categorical',
+    subset='training'
+)
+
+X_test = dataGen.flow_from_directory(
+    "beauty/",
+    target_size=(128,128),
+    batch_size=10,
+    class_mode='categorical',
+    subset='validation'
+)
+
+model.fit(
+    x = X_train,
+    validation_data=X_test,
+    steps_per_epoch=320,
+    epochs=50,
+    validation_steps=32,# 10 vezes menor que o steps_per_epoch
+    callbacks=[
+        callbacks.ModelCheckpoint(filepath='model/model.{epoch:02d}-{loss:.2f}.keras')
+    ]
+)
